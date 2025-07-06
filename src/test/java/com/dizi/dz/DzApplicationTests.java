8071e993b90ff900a82df15dfe8bac59fb185c09
pinkparrot-git <m2.raei30@gmail.com>
2025-07-06 09:12:43 +0200

    feat: Create Dizi-Cloud application

---
+++ src/test/java/com/dizi/dz/DzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizi.dz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
