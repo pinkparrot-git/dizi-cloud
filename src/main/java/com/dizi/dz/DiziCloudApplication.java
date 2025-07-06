0a77ae808e3296cd1a690e3a79220f84d959b740
pinkparrot-git <m2.raei30@gmail.com>
2025-07-06 16:47:49 +0200

    Create Dizi-Cloud application following the Spring Boot in Action book, covering up to Chapter 1.

---
+++ src/main/java/com/dizi/dz/DiziCloudApplication.java
@@ -0,0 +1,13 @@
+package com.dizi.dz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiziCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiziCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dizi/dz/DiziCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizi.dz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiziCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
